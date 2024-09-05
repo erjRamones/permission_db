8f33fd1c3a4183138305dfbf5900800cdb114aef
Sasas <zipppycole@gmail.com>
2024-09-05 13:28:24 +0800

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -89,8 +89,8 @@ class DatabaseSeeder extends Seeder
             'customer_group'
         ];
 
-        while ($c > 0) {
-            echo "1"; echo $f+1;
+        while ($this->c > 0) {
+            echo "1"; echo $this->f+1;
             $modeltype = $faker->randomElement($modelTypes);
             $description = $faker->sentence(10);
             echo $modeltype;
@@ -145,7 +145,7 @@ class DatabaseSeeder extends Seeder
                     throw new \InvalidArgumentException("Unknown model type: $modeltype");
             }
 
-            $c--;
+            $this->c--;
         }
 
         Employee::create([
