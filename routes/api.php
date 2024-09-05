8c53d3ce82f4eac5d5d7d38e0d1c28cf601d02c0
Sasas <zipppycole@gmail.com>
2024-09-05 18:14:34 +0800

    first commit

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,16 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 
 Route::middleware('auth:sanctum')->prefix('libraries')->group(function () {
     Route::get('/{modeltype}', [DBLibraryController::class, 'index']);
+    Route::get('/{modeltype}/{id}', [DBLibraryController::class,'show']);
     Route::post('/{modeltype}/{object}', [DBLibraryController::class, 'store']);
     Route::put('/{modeltype}/{id}/{object}', [DBLibraryController::class,'update']);
     Route::delete('/{modeltype}/{id}', [DBLibraryController::class, 'destroy']);
+});
+
+Route::middleware('auth:sanctum')->prefix('customers')->group(function () {
+    Route::get('/', [DBLibraryController::class, 'index']);
+    Route::get('/{id}', [DBLibraryController::class, 'show']);
+    Route::post('/', [DBLibraryController::class, 'store']);
+    Route::put('/{id}', [DBLibraryController::class,'update']);
+    Route::delete('/{id}', [DBLibraryController::class, 'destroy']);
 });
