a6b63ce1a45b6483bc78da7f0c54a894077f1928
Sasas <zipppycole@gmail.com>
2024-09-07 20:15:04 +0800

    first commit

--- app/Http/Requests/PersonalityUpdateRequest.php
+++ app/Http/Requests/PersonalityUpdateRequest.php
@@ -22,8 +22,32 @@ class PersonalityUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-                'firstname' => 'required',
-                'lastname' => 'required'
+            'first_name'                 => ['required', 'string'],
+            'family_name'                => ['required', 'string'],
+            'created_at'                 => ['required', 'date'],
+            'updated_at'                 => ['required', 'date'],
+            'datetime_registered'        => ['required', 'date'],
+            'name_type_code'             => ['required', 'integer'],
+            'middle_name'                => ['required', 'string'],
+            'description'                => ['required', 'string'],
+            'birthday'                   => ['required', 'date'],
+            'civil_status'               => ['required', 'integer'],
+            'gender_code'                => ['required', 'integer'],
+            'house_street'               => ['required', 'string'],
+            'purok_zone'                 => ['required', 'string'],
+            'postal_code'                => ['required', 'string'],
+            'telephone_no'               => ['required', 'string'],
+            'email_address'              => ['required', 'string'],
+            'cellphone_no'               => ['required', 'string'],
+            'personality_status_code'    => ['required', 'integer'],
+            'branch_id'                  => ['required', 'integer'],
+            'barangay_id'                => ['required', 'integer'],
+            'city_id'                    => ['required', 'integer'],
+            'country_id'                 => ['required', 'integer'],
+            'province_id'                => ['required', 'integer'],
+            'spouse_id'                  => ['required', 'integer'],
+            'credit_status_id'           => ['required', 'integer'],
+            'notes'                      => ['nullable', 'string'],
         ];
     }
 }
